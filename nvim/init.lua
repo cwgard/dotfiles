93ca24c3eabbf78190c7bd77db1354fcca381882
cwgard <cwgardner@gmail.com>
2024-03-03 16:43:42 -0500

    nvim: move remap's higher

--- nvim/init.lua
+++ nvim/init.lua
@@ -127,8 +127,49 @@ vim.opt.termguicolors = true
 
 vim.opt.isfname:append '@-@'
 
--- Vertical highlight for line length
 vim.opt.colorcolumn = '100'
+
+-- 03/01/24 CW: copy of remap.lua
+vim.keymap.set('n', '<leader>jk', vim.cmd.Ex)
+
+-- Move highlights lines up and down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- centers screen after half page
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- centers screen after search
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- deletes highligted word into void register and paste
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+
+-- delets into void register
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+
+-- potentially use Q for something. doing nothing as of now
+vim.keymap.set('n', 'Q', '<nop>')
+
+-- potentially use when using tmux
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+
+vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
@@ -780,45 +821,3 @@ require('lazy').setup {
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 --
-
--- 03/01/24 CW: copy of remap.lua
-vim.keymap.set('n', '<leader>jk', vim.cmd.Ex)
-
--- Move highlights lines up and down
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-
-vim.keymap.set('n', 'J', 'mzJ`z')
-
--- centers screen after half page
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-
--- centers screen after search
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
-
--- deletes highligted word into void register and paste
-vim.keymap.set('x', '<leader>p', [["_dP]])
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
-vim.keymap.set('n', '<leader>Y', [["+Y]])
-
--- delets into void register
-vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
-
--- potentially use Q for something. doing nothing as of now
-vim.keymap.set('n', 'Q', '<nop>')
-
--- potentially use when using tmux
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-
-vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
-
-vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
-vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
-vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
-vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
-
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
