71568c0965819f5bb748c1f49e2ae77c0a3cd0a0
cwgard <cwgardner@gmail.com>
2024-03-08 19:24:28 -0500

    nvim: netrw liststyle tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,6 +37,8 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
