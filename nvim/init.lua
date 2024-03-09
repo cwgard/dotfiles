8d969856ee27335cbadd1149a2b959ff025a5cf6
cwgard <cwgardner@gmail.com>
2024-03-09 10:44:35 -0500

    nvim: rm duplicate <leader>p binding"

--- nvim/init.lua
+++ nvim/init.lua
@@ -148,9 +148,6 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
--- deletes highligted word into void register and paste
-vim.keymap.set('x', '<leader>p', [["_dP]])
-
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
 vim.keymap.set('n', '<leader>Y', [["+Y]])
