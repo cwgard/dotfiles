2e3965b22011722bab1fb94fff6d0b74c86a2c5b
cwgard <cwgardner@gmail.com>
2024-03-19 20:48:48 -0400

    nvim: cleanup plugins

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,11 +1,10 @@
 return {
   'folke/which-key.nvim',
-  event = 'VeryLazy',   -- Sets the loading event to 'VeryLazy'
-  config = function()   -- This is the function that runs, AFTER loading
+  event = 'VeryLazy', -- Sets the loading event to 'VeryLazy'
+  config = function() -- This is the function that runs, AFTER loading
     require('which-key').setup()
-
-    -- Document existing key chains
     require('which-key').register {
+      -- Creates 'namespaces' for key maps without binding leader c, d, etc.
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
