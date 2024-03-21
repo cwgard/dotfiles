d270e80eac8371a93811a813d1ae45273f2f20df
cwgard <cwgardner@gmail.com>
2024-03-21 10:46:51 -0400

    df: center-bottom

---
+++ displayfusion/center-bottom-w50-h66-min.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Drawing;
+
+// The 'windowHandle' parameter will contain the window handle for the:
+//   - Active window when run by hotkey
+//   - Trigger target when run by a Trigger rule
+//   - TitleBar Button owner when run by a TitleBar Button
+//   - Jump List owner when run from a Taskbar Jump List
+//   - Currently focused window if none of these match
+public static class DisplayFusionFunction
+{
+	public static void Run(IntPtr windowHandle)
+	{
+		Rectangle workArea = BFS.Monitor.GetMonitorWorkAreaByWindow(windowHandle);
+
+		int newWidth = workArea.Width / 2;
+		int newHeight = (int)(workArea.Height * 0.6667);
+		int newX = workArea.X + (workArea.Width - newWidth) / 2;
+		int newY = workArea.Y + (workArea.Height - newHeight);
+
+		BFS.Window.SetSizeAndLocation(windowHandle, newX, newY, newWidth, newHeight);
+		BFS.Window.Minimize(windowHandle);
+	}
+}
